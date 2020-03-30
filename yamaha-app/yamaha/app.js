b2f6aa2ca0dcfc8db7a50a304d8d2415a597c5c6
jsrikrishna11 <41172782+jsrikrishna11@users.noreply.github.com>
2020-03-30 17:24:03 +0530

    Update app.js

--- yamaha-app/yamaha/app.js
+++ yamaha-app/yamaha/app.js
@@ -14,12 +14,13 @@ var config = {
 };
 var connection = sql.connect
 
+//Server setting
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+//Basic Routes
 app.get('/', function(req, res){
     res.render('pages/index');
 });
@@ -27,6 +28,7 @@ app.get('/', function(req, res){
 app.get('/save', function(req, res){
     res.send("Saved!")
 })
+
 app.get('/profile', function(req, res){
     res.render('pages/profile')
 })
@@ -38,32 +40,29 @@ app.post('/save', function(req, res){
     console.log(req.body)
     res.send("Thank you!")
 })
-app.get('/db', function(req, res){
-    
-   
-    // connect to your database
-    connection(config, function (err) {
-    
-        if (err) console.log(err);
 
-        // create Request object
+app.get("/askme", function(req, res){
+    res.render('pages/askme');
+});
+
+
+//DB access
+app.get('/db', function(req, res){
+    connection(config, function (err) {
+        if (err) console.log(err);
         var request = new sql.Request();
-            
-        // query to the database and get the records
         request.query('select * from insurance', function (err, recordset) {
             
             if (err) console.log(err)
-
-            // send records as a response
             res.send(recordset);
             
         });
     });
 });
+
 app.get('/existing', function(req, res){
     connection(config, function (err) {
         if (err) console.log(err);
-        // create Request object
         let statement="select policyID from insurance where county = \'UNION COUNTY\';"
         var request = new sql.Request();   
         request.query(statement, function (err, recordset) {
@@ -74,15 +73,10 @@ app.get('/existing', function(req, res){
                 console.log(i)
                 console.log(recordset["recordsets"][0][i])
             }
-        // send records as a response
             res.render('pages/dealers',{'selected':['policyID'],'data':recordset["recordsets"][0]});
     });
     })
 })
-
-app.get("/askme", function(req, res){
-    res.render('pages/askme');
-});
 
 app.post('/query', function(req, res){
     console.log(req.body);
@@ -92,22 +86,19 @@ app.post('/query', function(req, res){
     console.log(selection)
     console.log(column)
     connection(config, function (err) {
-
         if (err) console.log(err);
-        // create Request object
         let statement="";
         var request = new sql.Request();
         if(column=='county'|| column=='statecode' || column == "line"){
             value = "\'"+value+"\'" 
         }
         statement = 'select '+selection.join(',')+' from insurance where '+column+"="+value;    
-        // query to the database and get the records
         request.query(statement, function (err, recordset) {
             if (err) res.send("NO SUCH RECORD FOUND!")
-        // send records as a response
             res.render("pages/queryResult",{"selected": selection,"records":recordset["recordsets"][0]});
     });
 });
-
 })
+
+
 app.listen(8080, ()=>console.log("listening"));
